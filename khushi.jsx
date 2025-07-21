9d8ab26fc725aaa0ecd455c6aa307c10a07307f1
khushi0830 <khushisaini9588@gmail.com>
2025-07-21 21:35:24 +0530

    Create khushi.jsx

---
+++ khushi.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [name, setname] = useState('Dear')
+  const user = {
+    name: "Khushi",
+    Bio: "Learning frontend development from past 1.5 months"
+  }
+  let age = 22;
+  function sayhello() {
+    console.log("hello")
+  }
+  return (
+    <>
+      <Hero userDetails={user} data2={age} fun={sayhello}
+        name={name} setname={setname} />
+    </>
+  )
+}
+
+export default App
+
+function Hero({ userDetails, data2, fun, name, setname }) {
+  return (
+    <div>
+      <h1>hello {name}</h1>
+      <Card data={userDetails} setname={setname} />
+      <h3>my age, {data2}</h3>
+      <button onClick={() => fun()}>say hello</button>
+    </div>
+  )
+}
+
+function Card({ data, setname }) {
+  function changename() {
+    setname("Khushi");
+  }
+  return (
+    <>
+      <h2>Name: {data.name}</h2>
+      <h2>Bio: {data.Bio}</h2>
+      <button onClick={changename}>click me</button>
+    </>
+  )
+}
